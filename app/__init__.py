884b2513ed080d93f22be8916acb544b5630a44a
Fernando de Paz <f.depaz@hotmail.com>
2021-04-17 03:48:23 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -31,7 +31,7 @@ def add():
 
 @app.route("/temp.html", methods=["POST"])
 def temp():
-    print(flask.request.form.keys()[0])
+    print(flask.request.form.keys())
     global temperature
     temperature = flask.request.args.get('info')
     return flask.jsonify(
