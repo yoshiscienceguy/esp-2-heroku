39dd49fe32827a517c2358518293d01d751a9445
Fernando de Paz <f.depaz@hotmail.com>
2021-04-17 01:55:14 -0700

    added count

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,11 @@ from authlib.flask.client import OAuth
 
 app = flask.Flask(__name__)
 
+number = 0
+
 @app.route("/")
 def home_view():
-    return "<p>hello</p>"
+    return str(number)
 
 
 @app.route("/abc.html", methods=["GET"])
@@ -15,5 +17,12 @@ def abc():
                 message="good test",
                 category="success",
                 data="yes",
+                status=200
+            )
+
+@app.route("/add.html", methods=["POST"])
+def add():
+    number += 1
+    return flask.jsonify(
                 status=200
             )
