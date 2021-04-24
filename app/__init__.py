8d4983e4458f31d257a0aa2218de4b6b04686469
Fernando de Paz <f.depaz@hotmail.com>
2021-04-23 21:04:49 -0700

    updated vars

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,19 @@
-import flask,requests,json 
+import flask,requests,json, os
 import jinja2
 from authlib.flask.client import OAuth
 
 app = flask.Flask(__name__)
 
-number = 0
-temperature = 0
+
+
+
+os.environ['number'] = str(0)
+os.environ['temperature'] = str(0)
 
 @app.route("/")
 def home_view():
+    number = os.getenv('number')
+    temperature = os.getenv('temperature')
     return "number: " + str(number) +  " , temperature" + str(temperature)
 
 
@@ -23,20 +28,20 @@ def abc():
 
 @app.route("/add.html", methods=["POST"])
 def add():
-    global number
+    number = int(os.getenv('number'))
     number += 1
+    os.environ['number'] = str(number)
     return flask.jsonify(
                 status=200
             )
 
 @app.route("/temp.html", methods=["POST"])
 def temp():
-    global temperature
-    
+
     info = list(flask.request.form.keys())[0]
     
+    os.environ['temperature']  = info
     
-    temperature = info
     return flask.jsonify(
                 status=200
             )
